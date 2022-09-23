bfb1d1bd1adbb5514a0e58bcd19282626251ff60
Arsakan <Kittipong.Arsakan@analog.com>
2022-09-23 19:00:14 +0700

    angular inventory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './template/home/home.component';
+import { NavberComponent } from './template/navber/navber.component';
+import { DashboardComponent } from './template/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavberComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
